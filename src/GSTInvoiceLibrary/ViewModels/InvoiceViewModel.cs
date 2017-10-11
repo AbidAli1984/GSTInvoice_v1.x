4f22f79a2b475a5842c4980b30ca0b06ce2e02d9
ajay1507 <31920359+ajay1507@users.noreply.github.com>
2017-10-11 13:33:07 +0530

    Added Invoice Views and ViewModels

---
+++ src/GSTInvoiceLibrary/ViewModels/InvoiceViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GSTInvoiceData.ViewModels
+{
+    public class InvoiceViewModel
+    {
+        public Guid ClientId { get; set; }
+
+        public DateTime InvoiceDate { get; set; }
+
+        public int PONumber { get; set; }
+
+        public string Status { get; set; }
+
+        public string ClientName { get; set; }
+
+        public DateTime DueDate { get; set; }
+
+        public int Amount { get; set; }
+
+        public int Balance { get; set; }
+
+        public string DrorCr { get; set; }
+
+      
+    }
+}
